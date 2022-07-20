7d17e1779efe8a0039fa8befca3bd42ff7eee2d1
FedeTusa <fedetusa01@gmail.com>
2022-07-20 02:29:09 -0300

    borro algunas cosas del punto 8

--- p8/p8.c
+++ p8/p8.c
@@ -1,27 +1,3 @@
-/*
- * p8.c
- * 
- * Copyright 2022 Fede <fede@debian>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * gcc -Wall -o p8 p8.c -pthread   
- */
-
-
 #include <pthread.h>  
 #include <unistd.h>  
 #include <stdio.h>
@@ -107,7 +83,7 @@ void *sumoColumna (void *r) {
 	for(t=0;t<3;t++) {
 		subtotales[col] += matriz[t][col];
 	}
-	subtotales[col] += 1;  //nose xq me queda uno menos de lo que deberia
+	subtotales[col] += 1;  //queda uno menos de lo que deberia
 	sumadas++;
 	
 	if (sumadas >= 3) {
